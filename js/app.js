50923dd1cfba37e259b4a5b7592265316773c29a
Steven Cobb <stevecobb91@gmail.com>
2017-03-18 17:51:26 -0400

    vue examples

--- js/app.js
+++ js/app.js
@@ -6,3 +6,37 @@ var app = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue, dynamic text example!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
